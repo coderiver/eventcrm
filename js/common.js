f595dc7baa1a0380af8280e6a1e8affac99161e3
moroz <moroz@morozs-Mac-mini.local>
2013-12-18 15:14:08 +0200

    99%

--- js/common.js
+++ js/common.js
@@ -1,3 +1,33 @@
 $(document).ready(function() {
 
+//tooltip
+function tooltip() {
+	var el = $('.js-tooltip-wrap');
+	var btn = $('.js-tooltip');
+	var arr = el.find('.tooltip__arr');
+	btn.hover(function(){
+		var text = $(this).attr('data-tooltip');
+		el.find('.tooltip__text').html(text);
+		el.fadeIn(200);
+		var el_width = el.width();
+		var pos_top = $(this).offset().top;
+		var pos_left = $(this).offset().left;
+		var height = $(this).height();
+		var width = $(this).width();
+		var wnd_width = $(window).width();		
+		el.css({'top': pos_top + height});
+		if (wnd_width - pos_left < el_width) {
+			el.css({'left': pos_left + width - el_width + 1});
+			arr.css({'left': el_width - width/2 - 4});
+		}
+		else {
+			el.css({'left': pos_left + width/2 - el_width/2});
+			arr.css({'left': el_width/2 - 3});	
+		}		
+	},function(){
+		el.fadeOut(200);
+	});
+}
+tooltip();
+
 });
