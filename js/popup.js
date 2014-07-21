6712c9ce0dec1059d8f770f9874b585a2d346d6b
etna-scorpio <etna.scorpio@gmail.com>
2014-07-21 11:40:02 +0300

    small fixes phone mask is added

--- js/popup.js
+++ js/popup.js
@@ -2,4 +2,6 @@ $(document).ready(function() {
 	$('.dropdown select').change(function(event) {
 		$(this).next().val($(this).val());
 	});
+
+	$('.phone').inputmask("(999) 999-99-99",{placeholder:"x"});
 });
